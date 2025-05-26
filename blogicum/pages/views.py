b55fd2ce8a4d268975f3d9f4163a81545acb3f85
vovaovsyanka <vova_ovsyanka@mail.ru>
2025-05-26 04:16:49 +0300

    First7

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,30 +1,19 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Category, Post
+from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def index(request):
-    post_list = Post.objects.published().order_by('-pub_date')[:5]
-    return render(request, 'blog/index.html', {'post_list': post_list})
+class AboutView(TemplateView):
+    template_name = 'pages/about.html'
 
 
-def post_detail(request, id):
-    post = get_object_or_404(
-        Post.objects.published(),
-        pk=id
-    )
-    return render(request, 'blog/detail.html', {'post': post})
+class RulesView(TemplateView):
+    template_name = 'pages/rules.html'
 
+def csrf_failure(request, reason=''):
+    return render(request, 'pages/403csrf.html', status=403)
 
-def category_posts(request, category_slug):
-    category = get_object_or_404(
-        Category.objects.filter(is_published=True),
-        slug=category_slug
-    )
-    post_list = Post.objects.published().filter(
-        category=category
-    ).order_by('-pub_date')
-    return render(
-        request,
-        'blog/category.html',
-        {'category': category, 'post_list': post_list}
-    )
+def page_not_found(request, exception):
+    return render(request, 'pages/404.html', status=404)
+
+def server_error(request):
+    return render(request, 'pages/500.html', status=500)
